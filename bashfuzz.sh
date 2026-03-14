b22cbf243b9cf7a3c115aab795d6d099f0204d3f
SlippinJimmy13 <167783112+SlippinJimmy13@users.noreply.github.com>
2026-03-14 00:05:33 +0000

    Update bashfuzz.sh

--- bashfuzz.sh
+++ bashfuzz.sh
@@ -8,5 +8,5 @@ launch "feroxbuster" "feroxbuster --url $T --wordlist /home/kali/seclists/Discov
 launch "gobuster"    "gobuster dir --url $T --wordlist /home/kali/seclists/Discovery/Web-Content/raft-medium-directories.txt -b 404,502,503,429"
 launch "ffuf"        "ffuf -u $T/FUZZ -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -mc 200,301,302,403 -fc 502,503,429 -s"
  
-zenity --warning --title="Fuzzers running" --text=" " --ok-label="Kill All"
+zenity --warning --title="Fuzzers Running" --text=" " --ok-label="Kill All" 2>/dev/null
 kill "${PIDS[@]}" 2>/dev/null
