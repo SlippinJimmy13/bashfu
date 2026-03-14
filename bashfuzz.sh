33bdcf0b9f7d9d125630bafe0be222154b690ce7
SlippinJimmy13 <167783112+SlippinJimmy13@users.noreply.github.com>
2026-03-14 00:07:53 +0000

    Update bashfuzz.sh

--- bashfuzz.sh
+++ bashfuzz.sh
@@ -4,8 +4,8 @@ export XDG_RUNTIME_DIR=/tmp/runtime-root
 PIDS=()
 launch() { qterminal --title "$1" -e "bash -c '$2; read'" & PIDS+=($!); }
  
-launch "feroxbuster" "feroxbuster --url $T --wordlist /home/kali/seclists/Discovery/Web-Content/common.txt --filter-status 502,503,429"
-launch "gobuster"    "gobuster dir --url $T --wordlist /home/kali/seclists/Discovery/Web-Content/raft-medium-directories.txt -b 404,502,503,429"
+launch "feroxbuster" "feroxbuster --url $T --wordlist /usr/share/seclists/Discovery/Web-Content/common.txt --filter-status 502,503,429"
+launch "gobuster"    "gobuster dir --url $T --wordlist /usr/share/seclists/Discovery/Web-Content/raft-medium-directories.txt -b 404,502,503,429"
 launch "ffuf"        "ffuf -u $T/FUZZ -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -mc 200,301,302,403 -fc 502,503,429 -s"
  
 zenity --warning --title="Fuzzers Running" --text=" " --ok-label="Kill All" 2>/dev/null
